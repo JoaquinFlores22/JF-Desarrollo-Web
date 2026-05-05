aa6a316da56875d48ffb0e0fd168194f18e2279c
joaquinFlores.dev <107009830+JoaquinFlores22@users.noreply.github.com>
2026-05-05 18:54:56 -0300

    Update send-mail.js

--- api/send-mail.js
+++ api/send-mail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Solo se permiten peticiones POST' });
   }
